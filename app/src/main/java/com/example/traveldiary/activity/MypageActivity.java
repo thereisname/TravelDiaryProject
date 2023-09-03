a59ea44ed6dd25147df2b7025e745ba69770822e
thereisname <79213859+thereisname@users.noreply.github.com>
2023-09-03 22:48:42 +0900

    Add a process for retrieving user nicknames from DB.

--- app/src/main/java/com/example/traveldiary/activity/MypageActivity.java
+++ app/src/main/java/com/example/traveldiary/activity/MypageActivity.java
@@ -1,5 +1,6 @@
 package com.example.traveldiary.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -14,12 +15,15 @@ import com.example.traveldiary.R;
 import com.example.traveldiary.fragment.FragmentBoard;
 import com.example.traveldiary.fragment.FragmentBookmark;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MypageActivity extends AppCompatActivity {
     FragmentBoard fragmentBoard;
     FragmentBookmark fragmentBookmark;
-    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,19 @@ public class MypageActivity extends AppCompatActivity {
 
         fragmentBoard = new FragmentBoard();
         fragmentBookmark = new FragmentBookmark();
+
+        TextView nickName = findViewById(R.id.nickName);
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("UI");
+        mDatabase.child("users").child(userToken).child("userNickName").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                nickName.setText(String.valueOf(snapshot.getValue()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentBoard).commit();
         TabLayout tabs = findViewById(R.id.tabs);
