387b8aabc8724390c01b01a526e76c9fdc0a4923
JungSeungGeun <wjd15sheep@naver.com>
2023-09-21 20:38:07 +0900

    FragmentClient 이미지와 텍스트 불러오기 정말정말 끝

--- app/src/main/java/com/example/traveldiary/fragment/FragmentClient.java
+++ app/src/main/java/com/example/traveldiary/fragment/FragmentClient.java
@@ -1,5 +1,7 @@
 package com.example.traveldiary.fragment;
 
+import static java.lang.Thread.sleep;
+
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,12 +15,14 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.traveldiary.R;
 import com.example.traveldiary.value.MyPageValue;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
@@ -47,13 +51,12 @@ public class FragmentClient extends Fragment {
     private MyPageValue mp;
 
     private LinearLayout listView;
-
-    private ArrayList<Uri> imagess = new ArrayList<Uri>();
+    ImageView imageView;
 
     int num = 0;
     ArrayList<Integer> arrayStartIndex = new ArrayList<Integer>();
     ArrayList<Integer> arrayEndIndex = new ArrayList<Integer>();
-
+    ArrayList<View> arrayimage = new ArrayList();
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -66,25 +69,23 @@ public class FragmentClient extends Fragment {
         TextView fragment_title = view.findViewById(R.id.fragment_title);
         TextView fragment_hashtag = view.findViewById(R.id.fragment_hashtag);
 
-
+        Log.d(TAG, "확인하는중");
 
 
         listView = view.findViewById(R.id.listView);
-        db.collection("data").whereNotEqualTo("userToken", getUserToken()).limit(1).get().addOnSuccessListener(queryDocumentSnapshots -> {
+        db.collection("data").whereNotEqualTo("userToken", FirebaseAuth.getInstance().getUid()).limit(1).get().addOnSuccessListener(queryDocumentSnapshots -> {
             for (QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots) {
                 mp = queryDocumentSnapshot.toObject(MyPageValue.class);
-
+                checkText(mp);
                 fragment_title.setText(mp.getTitle());
                 fragment_hashtag.setText(mp.getHashTag());
-
-
-                checkText(mp);
 
 
             }
 
 
         });
+
 
         return view;
     }
@@ -102,67 +103,88 @@ public class FragmentClient extends Fragment {
             }
         }
         // 이미지 가져오기
-        Imagedown(mp.getBoardID(), arrayStartIndex, arrayEndIndex);
+        // Imagedown(mp.getBoardID(), arrayStartIndex, arrayEndIndex);
+
+        if(arrayStartIndex.size() == 0){
+            createTextView(mp.getCon());
+        }else{
+            if (arrayStartIndex.get(0) != 0) {
+                String str0 = mp.getCon().substring(0, arrayStartIndex.get(0));
+                createTextView(str0);
+                for (int i = 0; i < arrayStartIndex.size(); i++) {
+                    createImageView();
+                    if(i == arrayStartIndex.size()-1){
+                        String str1 = mp.getCon().substring(arrayEndIndex.get(i)+1, mp.getCon().length());
+                        createTextView(str1);
+
+                    }else{
+                        String str1 = mp.getCon().substring(arrayEndIndex.get(i)+1, arrayStartIndex.get(i+1));
+                        createTextView(str1);
+                    }
+                }
+            } else {
+                for (int i = 0; i < arrayStartIndex.size(); i++) {
+                    createImageView();
+                    if(i == arrayStartIndex.size()-1){
+                        String str2 = mp.getCon().substring(arrayEndIndex.get(i)+1, mp.getCon().length());
+                        createTextView(str2);
+
+                    }else{
+                        String str3 = mp.getCon().substring(arrayEndIndex.get(i)+1, arrayStartIndex.get(i+1));
+                        createTextView(str3);
+                    }
+                }
+            }
+        }
+
+        try{
+            sleep(1000);
+            Imagedown(mp.getBoardID());
+        }catch (Exception e){
+
+        }
 
         Log.d(TAG, "시작과 끝 배열 크기 " + arrayEndIndex.size() + arrayStartIndex.size());
     }
 
     // Image 다운로드 함수
-    private void Imagedown(String boardID, ArrayList<Integer> arrayStartIndex, ArrayList<Integer> arrayEndIndex) {
+    private void Imagedown(String boardID) {
+
         StorageReference storageReference = FirebaseStorage.getInstance().getReference();
         storageReference.child("/Image/" + boardID).listAll().addOnSuccessListener(listResult -> {
 
-
-            for (StorageReference item : listResult.getItems()) {
+            int a = listResult.getItems().size();
+            Log.d(TAG, "우리확인 하자" + a);
+            for(int i = 0; i < listResult.getItems().size(); i++ ){
+                StorageReference item = listResult.getItems().get(i);
+                int finalI = i;
                 item.getDownloadUrl().addOnSuccessListener(command -> {
-                    imagess.add(command);
                     Log.d("로그1", String.valueOf(command) + "uri가져오기");
-                    Log.d("로그1", String.valueOf(imagess.size()) + "첫 번째 imagess 사이지 확인");
-                    Log.d("로그1", imagess.get(0).toString() + "imagess 인덱스 첫번째 가져오기");
-
-                    imagess.add(command);
-                    Log.d(TAG, "이미지 배열 첫번째 확인" + arrayStartIndex.get(0));
-                    Log.d(TAG, String.valueOf( "이미지 배열 사이즈 확인" + arrayStartIndex.size()));
-
-
-                    if (arrayStartIndex.get(0) != 0) {
-                        String str = mp.getCon().substring(0, arrayStartIndex.get(0));
-                        createTextView(str);
-                    } else {
-                        for (int i = 0; i < arrayStartIndex.size(); i++) {
-                            createImageView(imagess.get(i));
-                            if(i == arrayStartIndex.size()-1){
-                                String str = mp.getCon().substring(arrayEndIndex.get(i)+1, mp.getCon().length());
-                                createTextView(str);
-
-                            }else{
-                                String str = mp.getCon().substring(arrayEndIndex.get(i)+1, arrayStartIndex.get(i+1));
-                                createTextView(str);
-                            }
-
-
-
-                        }
-
-                    }
+                    Glide.with(getContext()).load(command).into(((ImageView) arrayimage.get(finalI)));
+                    Log.d(TAG, "이미지 크기 가져오기" + listResult.getItems());
                 });
             }
 
 
         }).addOnFailureListener(command -> Log.d("error", "불러오기 실패."));
+
     }
 
-    private void createImageView(Uri img) {
+    int imageId = 10000;
+    private void createImageView() {
         Log.d("로그", "뷰 확인");
-        ImageView imageView = new ImageView(getContext());
-        Glide.with(getContext()).load(img).into(imageView);
+        imageView = new ImageView(getContext());
+        imageView.setId(imageId);
+        int b =  imageView.getId();
+        Glide.with(getContext()).load(R.drawable.baseline_image_24).into(imageView);
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         imageView.setLayoutParams(param);
-
+        Log.d(TAG, "이미지 뷰 들어갔는지 확인"+ String.valueOf(b));
+        arrayimage.add(imageView);
+        Log.d(TAG, "이미지 뷰 들어갔는지 확인"+ arrayimage.size());
         listView.addView(imageView);
+        imageId++;
     }
-
-    int count = 0;
 
     private void createTextView(String str) {
         TextView textViewNm = new TextView(getContext());
@@ -170,12 +192,13 @@ public class FragmentClient extends Fragment {
         textViewNm.setTextSize(15);
         textViewNm.setTextColor(Color.rgb(0, 0, 0));
         textViewNm.setBackgroundColor(Color.rgb(184, 236, 184));
-        textViewNm.setId(count);
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         textViewNm.setLayoutParams(param);
         listView.addView(textViewNm);
         Log.d("로그", String.valueOf(textViewNm.getId() + "textview id"));
-        count++;
+
     }
 
+
 }
+
